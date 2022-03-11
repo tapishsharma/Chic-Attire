ce8358b5c523e26133c8ac99fbf2e2911e39876e
tapishsharma <tapishmail@gmail.com>
2022-03-11 10:02:38 +0530

    Sign up added with firebase authentication

--- src/App.js
+++ src/App.js
@@ -1,62 +1,64 @@
-import React from 'react';
-import './App.css';
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-  Link,
-  
-} from "react-router-dom";
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import {HomePage} from './pages/homepage/homepage.component';
-import ShopPage from './pages/shop/shop.component'
-import Header from './components/header/header.component';
-import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up';
-import {auth} from './firebase/firebase.utils';
-
+import { HomePage } from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
+import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
 class App extends React.Component {
-  constructor(){
+  constructor() {
     super();
-    this.state={
-      currentUser:null
-    }
-  }
-  unsubscribeFromAuth=null;
-  componentDidMount(){
-   this.unsubscribeFromAuth=  auth.onAuthStateChanged(user=>{
-        this.setState({currentUser:user});
-     })
-  }
-componentWillUnmount(){
-  this.unsubscribeFromAuth();
-}
 
-  render(){
+    this.state = {
+      currentUser: null
+    };
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          });
+
+          console.log(this.state);
+        });
+      }
+
+      this.setState({ currentUser: userAuth });
+    });
+  }
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+  render() {
     return (
-<div>
-<BrowserRouter>
-<Header currentUser={this.state.currentUser}/>
+      <div>
+        <BrowserRouter>
+          <Header currentUser={this.state.currentUser} />
 
-      <Routes>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
 
-  
-
-  <Route  path="/" element={<HomePage />}/>
-        
-  <Route path="/shop" element={<ShopPage />} />
-  <Route path="/signin" element={<SignInAndSignUp />} />
-
-      </Routes>
-    
-</BrowserRouter>
-
-
-</div>
-
-  );
+            <Route path="/shop" element={<ShopPage />} />
+            <Route path="/signin" element={<SignInAndSignUp />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    );
   }
-  
 }
 
 export default App;
